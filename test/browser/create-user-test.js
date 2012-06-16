7e2a6c768399e37eea6e2be9a84cf1d6eb4c3ba1
Daniel Brolund <daniel.brolund@agical.com>
2012-06-16 17:52:35 +0200

    Moved login to function

--- test/browser/create-user-test.js
+++ test/browser/create-user-test.js
@@ -36,6 +36,35 @@ function createTestBrowser(done) {
   return client;
 }
 
+function loginCreatedUser(done) {
+  var whenBrowser = when.defer();
+  
+  createNewUser("genUser" + new Date().getTime().toString(), "1234568", function(err,user) {
+    if(err) {assert.fail(err); return;}
+    var browser = createTestBrowser(done);
+    browser
+      .init()
+      .url("http://localhost:8000/")
+      .setValue("#username", user.username)
+      .click("#do-login")
+      .pause(100)
+      .windowHandles(function(data){
+        var popupWindow = data.value[1];
+        console.log("popupWindow is", popupWindow);
+        this.window(popupWindow)
+          .waitFor('input[name="password"]', 500, function(){}) 
+          .setValue('input[name="password"]', user.password)
+          .submitForm("form")
+          .windowHandles(function(data){
+              var originalWindow = data.value[0];
+              whenBrowser.resolve(
+                {browser: this.window(originalWindow).cssEq("#welcome", "Welcome, " + user.username + "!"),
+                 loggedInUser: user});
+          })});
+      });
+  return whenBrowser.promise;
+}
+
 function createNewUser(username, password, cb) {
   var options = {
     host: 'localhost',
@@ -67,34 +96,11 @@ function createNewUser(username, password, cb) {
     
     "can login a user": function (done) {
         this.timeout = 25000;
-        var whenBrowser = when.defer();
-        
-        
-        
-        createNewUser("genUser" + new Date().getTime().toString(), "1234568", function(err,user) {
-          if(err) {assert.fail(err); return;}
-          var browser = createTestBrowser(done);
-          browser
-            .init()
-            .url("http://localhost:8000/")
-            .setValue("#username", user.username)
-            .click("#do-login")
-            .pause(100)
-            .windowHandles(function(data){
-              var popupWindow = data.value[1];
-              console.log("popupWindow is", popupWindow);
-              this.window(popupWindow)
-                .waitFor('input[name="password"]', 500, function(){}) 
-                .setValue('input[name="password"]', user.password)
-                .submitForm("form")
-                .windowHandles(function(data){
-                    var originalWindow = data.value[0];
-                    whenBrowser.resolve(this.window(originalWindow)
-                      .cssEq("#welcome", "Welcome, " + user.username + "!"));
-                })});
-            });
-        
-        whenBrowser.promise.then(function(b) {b.end(done);});
+         
+        loginCreatedUser(done).then(function(browserAndUser) {
+          browserAndUser.browser
+            .end(done);
+        });
     },
     
     
