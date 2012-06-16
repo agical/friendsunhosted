de4aa954a1242061d997af978b8ade8c5750d50b
Daniel Brolund <daniel.brolund@agical.com>
2012-06-16 21:48:58 +0200

    Removed some unnecessary function declarations

--- test/browser/create-user-test.js
+++ test/browser/create-user-test.js
@@ -39,28 +39,29 @@ function createTestBrowser(done) {
 function loginCreatedUser(done) {
   var whenBrowser = when.defer();
   
-  createNewUser("genUser" + new Date().getTime().toString(), "1234568").then(function(user) {
-    var browser = createTestBrowser(done);
-    browser
-      .init()
-      .url("http://localhost:8000/")
-      .setValue("#username", user.username)
-      .click("#do-login")
-      .pause(100)
-      .windowHandles(function(data){
-        var popupWindow = data.value[1];
-        console.log("popupWindow is", popupWindow);
-        this.window(popupWindow)
-          .waitFor('input[name="password"]', 500, function(){}) 
-          .setValue('input[name="password"]', user.password)
-          .submitForm("form")
-          .windowHandles(function(data){
-              var originalWindow = data.value[0];
-              whenBrowser.resolve(
-                {browser: this.window(originalWindow),
-                 loggedInUser: user});
-          })});
-      }, function(err){assert.fail(err);});
+  createNewUser("genUser" + new Date().getTime().toString(), "1234568")
+    .then(function(user) {
+      var browser = createTestBrowser(done);
+      browser
+        .init()
+        .url("http://localhost:8000/")
+        .setValue("#username", user.username)
+        .click("#do-login")
+        .pause(100)
+        .windowHandles(function(data) {
+          var popupWindow = data.value[1];
+          console.log("popupWindow is", popupWindow);
+          this.window(popupWindow)
+            .waitFor('input[name="password"]', 500) 
+            .setValue('input[name="password"]', user.password)
+            .submitForm("form")
+            .windowHandles(function(data){
+                var originalWindow = data.value[0];
+                whenBrowser.resolve(
+                  {browser: this.window(originalWindow),
+                   loggedInUser: user});
+            })});
+    }, assert.fail);
   return whenBrowser.promise;
 }
 
@@ -76,9 +77,7 @@ function createNewUser(username, password, cb) {
     deferred.resolve(
       {username: username + "@" + options.host,
        password: password });
-  }).on('error', function(e) {
-      deferred.reject(e);
-  });
+  }).on('error', deferred.reject);
     
   return deferred.promise;
 }
