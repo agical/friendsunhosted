354f11533595a229921be8755ea6b2b999e8c3f1
Daniel Brolund <daniel.brolund@agical.com>
2012-07-14 23:36:26 +0200

    Restructured remote adapter

--- site/js/main.js
+++ site/js/main.js
@@ -2,11 +2,62 @@ require(['jquery', 'underscore', 'ui', 'ko', 'remoteStorage', 'when'], function(
 
     var daStore = function() {
         var val = {};
-        var getUserStorageClient = function(category) {
+
+        function connect(userAddress, callback) {
+            remoteStorage.getStorageInfo(userAddress, function(error, storageInfo) {
+              if(error) {
+                alert('Could not load storage info:' + error);
+                console.log(error);
+              } else {
+                console.log('Storage info received:');
+                console.log(storageInfo);
+              }
+
+              callback(error, storageInfo);
+            });
+        };
+
+        function getUserStorageClient(category) {
             var token = localStorage.getItem('bearerToken');
             var storageInfo = JSON.parse(localStorage.getItem('userStorageInfo'));
             var client = remoteStorage.createClient(storageInfo, category, token);
             return client;      
+        };
+
+        function authorize(categories) {
+            var storageInfo = JSON.parse(localStorage.getItem('userStorageInfo'));
+            var redirectUri = location.protocol + '//' + location.host + '/receive_token.html';
+
+            var oauthPage = remoteStorage.createOAuthAddress(storageInfo, categories, redirectUri);
+            window.open(oauthPage); //returns popup
+        };
+
+        val.login = function(username) {
+            var deferred = when.defer();
+            connect(username, function(err, storageInfo) {
+                  if(err) {
+                      deferred.reject(err);
+                  } else {
+                      localStorage.setItem('username', JSON.stringify(username));
+                      localStorage.setItem('userStorageInfo', JSON.stringify(storageInfo));
+                      authorize(['public']);
+                  }
+            });
+            return deferred.promise;
+        };
+
+        val.init = function() {
+            var deferred = when.defer();
+            
+            var localUsername = localStorage.getItem('username');
+            var token = localStorage.getItem('bearerToken');
+            
+            if(localUsername && token) {
+                deferred.resolve(JSON.parse(localUsername));
+            } else {
+                deferred.reject("No logged in user");
+            }
+            return deferred.promise;
         };
       
         val.fetchUserData = function(key, category) {
@@ -62,41 +113,31 @@ require(['jquery', 'underscore', 'ui', 'ko', 'remoteStorage', 'when'], function(
             return deferred.promise();
         };
 
-        function connect(userAddress, callback) {
-            remoteStorage.getStorageInfo(userAddress, function(error, storageInfo) {
-              if(error) {
-                alert('Could not load storage info:' + error);
-                console.log(error);
-              } else {
-                console.log('Storage info received:');
-                console.log(storageInfo);
-              }
-
-              callback(error, storageInfo);
-            });
-        };
-
-        var authorize = function(categories) {
-            var storageInfo = JSON.parse(localStorage.getItem('userStorageInfo'));
-            var redirectUri = location.protocol + '//' + location.host + '/receive_token.html';
-
-            var oauthPage = remoteStorage.createOAuthAddress(storageInfo, categories, redirectUri);
-            var popup = window.open(oauthPage);
-        };
-
-        val.init = function() {
+        val.logout = function() {
             var deferred = when.defer();
-            
-            var localUsername = localStorage.getItem('username');
-            var token = localStorage.getItem('bearerToken');
-            
-            if(localUsername && token) {
-                deferred.resolve(JSON.parse(localUsername));
-            } else {
-                deferred.reject("No logged in user");
-            }
+            localStorage.clear('username');
+            localStorage.clear('userStorageInfo');
+            localStorage.clear('bearerToken');
+            deferred.resolve();
             return deferred.promise;
         };
+
+        val.deleteUserData = function(key) {
+            var deferred = when.defer();
+            
+            var client = getUserStorageClient('public');
+
+            client.delete(key, function(err) {
+              if(err) {
+                deferred.reject(err);
+              } else {
+                deferred.resolve(key);
+              }
+        });
+          
+        return deferred.promise;
+    };
+
 
         window.addEventListener('message', function(event) {
             if(event.origin == location.protocol +'//'+ location.host) {
@@ -107,44 +148,7 @@ require(['jquery', 'underscore', 'ui', 'ko', 'remoteStorage', 'when'], function(
         }, false);
 
           
-        val.login = function(username) {
-              var deferred = when.defer();
-              connect(username, function(err, storageInfo) {
-                    if(err) {
-                        deferred.reject(err);
-                    } else {
-                        localStorage.setItem('username', JSON.stringify(username));
-                        localStorage.setItem('userStorageInfo', JSON.stringify(storageInfo));
-                        authorize(['public']);
-                    }
-              });
-              return deferred.promise;
-        };
 
-        val.logout = function() {
-              var deferred = when.defer();
-              localStorage.clear('username');
-              localStorage.clear('userStorageInfo');
-              localStorage.clear('bearerToken');
-              deferred.resolve();
-              return deferred.promise;
-        };
-
-        val.deleteUserData = function(key) {
-              var deferred = when.defer();
-              
-              var client = getUserStorageClient('public');
-
-              client.delete(key, function(err) {
-                if(err) {
-                  deferred.reject(err);
-                } else {
-                  deferred.resolve(key);
-                }
-              });
-              
-              return deferred.promise;
-        };
 
         return val;
     };
