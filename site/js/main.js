58261f2f721f28f66e7c910d755fcd84facb8cfb
Daniel Brolund <daniel.brolund@agical.com>
2012-07-18 10:45:59 +0200

    Improved local speed by replacing flattr button with a static button and putting all sponsor button on our server

--- site/js/main.js
+++ site/js/main.js
@@ -222,17 +222,8 @@ require(['jquery', 'underscore', 'ui', 'ko', 'when', 'remoteAdapter'], function(
     init();
   };
 
-    var initFlattr = function() {
-        var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
-        s.type = 'text/javascript';
-        s.async = true;
-        s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto';
-        t.parentNode.insertBefore(s, t);
-    };
-      
     $(function(){
         ko.applyBindings(new FriendsViewModel());
-        setTimeout(initFlattr, 0);
     });
   
 });
