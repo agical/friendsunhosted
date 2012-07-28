9d1bc7b40705b3ab795ff5ebf9dd3b66ac3eadf1
Daniel Brolund <daniel.brolund@agical.com>
2012-07-28 19:29:18 +0200

    Running both unit and functional tests

--- test/buster.js
+++ test/buster.js
@@ -1,13 +1,12 @@
 var config = module.exports;
 
-/*
 config["functional-selenium"] = {
     environment: "node",
     rootPath: "../",
     tests: ["test/browser/**" + "/*-test.js"],
     //resources: ["**" + "/*.js"],
 };
-*/
+
 config["unit"] = {
        // id:null,
         environment: "browser",
