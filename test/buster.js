d6d07ce5fbe65ce5422bc78628c98a0943cc1a0e
Daniel Brolund <daniel.brolund@agical.com>
2012-07-28 19:30:54 +0200

    Changed indentation

--- test/buster.js
+++ test/buster.js
@@ -7,22 +7,22 @@ config["functional-selenium"] = {
 };
 
 config["unit"] = {
-        environment: "browser",
-        rootPath: "..",
-        sources: [
-            "site/js/**/*.js"
-        ],
-        tests: [
-            "test/unit/**/*-test.js"
-        ],
-        libs: [
-            "test/require-config.js",
-            "test/require-jquery.js"
-        ],
-        extensions: [
-            require("buster-amd")
-        ],
-    };
+    environment: "browser",
+    rootPath: "..",
+    sources: [
+        "site/js/**/*.js"
+    ],
+    tests: [
+        "test/unit/**/*-test.js"
+    ],
+    libs: [
+        "test/require-config.js",
+        "test/require-jquery.js"
+    ],
+    extensions: [
+        require("buster-amd")
+    ],
+};
 
 
 
