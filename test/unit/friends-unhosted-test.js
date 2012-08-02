10fcb0ba04f632a48354e838d38f86e51a96da2a
Daniel Brolund <daniel.brolund@agical.com>
2012-08-02 22:40:56 +0200

    Handles no data

--- test/unit/friends-unhosted-test.js
+++ test/unit/friends-unhosted-test.js
@@ -3,46 +3,8 @@ function(fu, ra, when, help) {
     var eq = help.eq;
     var resolved = help.resolved;
     var rejected = help.rejected;
-    
-    function testOldStore(o, version, done) {
-        ra.getPublicData = o.stub();
-        var data = {'user': 'data'};
-        ra.getPublicData
-            .withArgs('some@user.com', 'VERSION')
-            .returns(resolved(version));
-        ra.getPublicData
-            .withArgs('some@user.com', 'friendsunhosted_statusupdate_testing')
-            .returns(resolved(data));
         
-        fu.fetchStatusForUser('some@user.com').then(eq(data),eq(data)).then(done, done);
-    }
-    
     buster.testCase("F#U API read public data", {
-
-        "//- can read version 0 status updates": function(done) {
-            testOldStore(this, null, done);
-        },
-
-        "//- can read version 1 status updates": function(done) {
-            testOldStore(this, 1, done);
-        },
-
-        "//- can read version 2 status updates": function(done) {
-            testOldStore(this, 2, done);
-        },
-
-        "//- can read version 3 status updates": function(done) {
-            ra.getPublicData = this.stub();
-            var data = {'user': 'data'};
-            ra.getPublicData
-                .withArgs('some@user.com', 'VERSION')
-                .returns(resolved(3));
-            ra.getPublicData
-                .withArgs('some@user.com', 'friendsunhosted_status')
-                .returns(resolved(data));
-            
-            fu.fetchStatusForUser('some@user.com').then(eq(data),eq(data)).then(done, done);
-        },
 
         "- reads only old updates": function(done) {
             ra.getPublicData = this.stub();
