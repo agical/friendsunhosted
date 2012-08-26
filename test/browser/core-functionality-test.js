a08283d079fabe0035ba0743e5dde8e5bfc9a02b
Daniel Brolund <daniel.brolund@agical.com>
2012-08-26 16:09:32 +0200

    All tests working with the new dialogs

--- test/browser/core-functionality-test.js
+++ test/browser/core-functionality-test.js
@@ -33,7 +33,7 @@ var assertVisible = function() {
 
 
 buster.testCase("Friends#Unhosted", {
-    "//- has a title and info on load": function (done) {
+    "- has a title and info on load": function (done) {
         this.timeout = 5000;
         
         createRobot(done)   
@@ -43,7 +43,7 @@ buster.testCase("Friends#Unhosted", {
         .end();
     },
     
-    "//- can login a user": function (done) {
+    "- can login a user": function (done) {
         this.timeout = 25000;
             
         createRobot(done)  
@@ -71,7 +71,7 @@ buster.testCase("Friends#Unhosted", {
         .end();
     },
 
-    "//- can comment on status updates": function (done) {
+    "- can comment on status updates": function (done) {
         this.timeout = 25000;
         createRobot(done)  
             .loginNewUser()
@@ -83,7 +83,7 @@ buster.testCase("Friends#Unhosted", {
         .end();
     },
 
-    "//- can collapse and expand conversations": function (done) {
+    "- can collapse and expand conversations": function (done) {
         this.timeout = 25000;
         createRobot(done)  
             .loginNewUser()
@@ -108,7 +108,7 @@ buster.testCase("Friends#Unhosted", {
         .end();
     },
 
-    "//- emails and links are clickables": function (done) {
+    "- emails and links are clickables": function (done) {
         this.timeout = 25000;
 
         createRobot(done)  
@@ -138,7 +138,7 @@ buster.testCase("Friends#Unhosted", {
         .end();
     },
     
-    "//- can let user add, list and remove friends": function (done) {
+    "- can let user add, list and remove friends": function (done) {
         this.timeout = 25000;
         
         createTestUser().then(function(userToBeAdded) {
@@ -162,7 +162,7 @@ buster.testCase("Friends#Unhosted", {
         
     },
 
-    "//- user can see friends of friends and add them": function (done) {
+    "- user can see friends of friends and add them": function (done) {
         this.timeout = 25000;
 
         function createFriendWithFriend() {
@@ -205,7 +205,7 @@ buster.testCase("Friends#Unhosted", {
         
     },
 
-    "//- can let user see friends messages": function (done) {
+    "- can let user see friends messages": function (done) {
         this.timeout = 25000;
 
         var waitForUserAddingStatus = when.defer();
@@ -236,7 +236,7 @@ buster.testCase("Friends#Unhosted", {
         });
     },
 
-    "//- keeps login status on refresh": function (done) {
+    "- keeps login status on refresh": function (done) {
         this.timeout = 25000;
         
         createRobot(done).loginNewUser()
@@ -247,7 +247,7 @@ buster.testCase("Friends#Unhosted", {
         .end();
     },
 
-    "//- can logout user": function (done) {
+    "- can logout user": function (done) {
         this.timeout = 25000;
         
         createRobot(done).loginNewUser()
@@ -258,7 +258,7 @@ buster.testCase("Friends#Unhosted", {
         .end();
     },
 
-    "//- shows latest activity on top": function (done) {
+    "- shows latest activity on top": function (done) {
         this.timeout = 25000;
          
         createRobot(done).loginNewUser()
@@ -273,7 +273,7 @@ buster.testCase("Friends#Unhosted", {
         .end();               
     },
 
-    "//- can write to directly to store": function (done) {
+    "- can write to directly to store": function (done) {
         this.timeout = 25000;
 
         var _username = 'mongo@localhost';
@@ -297,7 +297,7 @@ buster.testCase("Friends#Unhosted", {
             });
     },
 
-    "//- can see other participants in threads": function (done) {
+    "- can see other participants in threads": function (done) {
         this.timeout = 25000;
 
         var _category = 'public';
@@ -335,7 +335,7 @@ buster.testCase("Friends#Unhosted", {
     },
 
     
-    "//- display 3 days at a time with a fetch more button": function (done) {
+    "- display 3 days at a time with a fetch more button": function (done) {
         this.timeout = 25000;
 
         var _username = 'mongo@localhost';
