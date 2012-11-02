a912838f3426e837824a9e26857c0d1af7afa482
Daniel Brolund <daniel.brolund@agical.com>
2012-11-02 10:44:17 +0100

    Reformatted code

--- test/unit/friends-unhosted-api-friends-test.js
+++ test/unit/friends-unhosted-api-friends-test.js
@@ -1,84 +1,96 @@
-define(['friendsUnhostedCode', 'underscore', 'when', 'remoteAdapter', 'testHelper'], 
-function(fuc, _, when, remoteAdapter, help) {
-    var eq = help.eq;
-    var match = help.match;
-    var resolved = help.resolved;
-    var rejected = help.rejected;
+define(['friendsUnhostedCode', 'underscore', 'when', 'remoteAdapter', 'testHelper'],
+    function (fuc, _, when, remoteAdapter, help) {
+        var eq = help.eq;
+        var match = help.match;
+        var resolved = help.resolved;
+        var rejected = help.rejected;
 
-    var fu = null;
-    var ra = null;
-    var fakeDialog;
-    
-    function setUpRemoteAdapterAndFuApi() {
-        ra = this.mock(remoteAdapter);
-        fakeDialog = {};
-        fu = fuc(_, when, ra.object, fakeDialog);
-    }    
+        var fu = null;
+        var ra = null;
+        var fakeDialog;
 
-    buster.testCase("F#U API friends management", {
-        setUp: setUpRemoteAdapterAndFuApi,
-        
-        "- fetch friends of friend": function(done) {
-            var friends = [{'username': 'test@agical.com', 'timestamp':9876543210},
-                          {'username': 'fersuch@agical.com', 'timestamp':9876543211}];
+        function setUpRemoteAdapterAndFuApi() {
+            ra = this.mock(remoteAdapter);
+            fakeDialog = {};
+            fu = fuc(_, when, ra.object, fakeDialog);
+        }
 
-            ra.expects('getPublicData')
-                .withArgs('some@user.com', 'friendsunhosted_friends')
-                .returns(resolved(friends));
+        buster.testCase("F#U API friends management", {
+            setUp:setUpRemoteAdapterAndFuApi,
 
-            fu.fetchFriendsOfFriend('some@user.com').always(eq(friends)).always(done);
-        },
+            "- fetch friends of friend":function (done) {
+                var friends = [
+                    {'username':'test@agical.com', 'timestamp':9876543210},
+                    {'username':'fersuch@agical.com', 'timestamp':9876543211}
+                ];
 
-        "- cannot add same user twice": function(done) {
-            var friends = [{'username': 'some@user.com', 'timestamp':9876543210}];
+                ra.expects('getPublicData')
+                    .withArgs('some@user.com', 'friendsunhosted_friends')
+                    .returns(resolved(friends));
 
-            ra.expects('fetchUserData')
-                .withExactArgs('friendsunhosted_friends')
-                .returns(resolved(friends));
-            
-            fakeDialog.info = function(message) {
-                assert.defined(message);
-                var ret = when.defer();
-                ret.resolve();
-                return ret.promise;
-            };
+                fu.fetchFriendsOfFriend('some@user.com').always(eq(friends)).always(done);
+            },
 
-            fu.addFriend('some@user.com').always(eq(friends[0].username + " is already your friend!")).always(done);
-        },
-        
-        "- Puts new friend for no friends in repo": function(done) {
-            this.originalGetTimestamp = fu.getTimestamp;
-            fu.getTimestamp = function() {return 123456789;};
+            "- cannot add same user twice":function (done) {
+                var friends = [
+                    {'username':'some@user.com', 'timestamp':9876543210}
+                ];
 
-            var friends = [{'username': 'test@agical.com', 'timestamp':123456789}];
+                ra.expects('fetchUserData')
+                    .withExactArgs('friendsunhosted_friends')
+                    .returns(resolved(friends));
 
-            ra.expects('fetchUserData')
-                .withExactArgs('friendsunhosted_friends')
-                .returns(resolved(null));
-            
-            ra.expects('putUserData')
-                .withArgs('friendsunhosted_friends', friends)
-                .returns(resolved(friends));
-            
-            fu.addFriend(friends[0].username).then(eq(friends[0]), eq('fail')).always(done);
-            
-        },
+                fakeDialog.info = function (message) {
+                    assert.defined(message);
+                    var ret = when.defer();
+                    ret.resolve();
+                    return ret.promise;
+                };
+
+                fu.addFriend('some@user.com').always(eq(friends[0].username + " is already your friend!")).always(done);
+            },
+
+            "- Puts new friend for no friends in repo":function (done) {
+                this.originalGetTimestamp = fu.getTimestamp;
+                fu.getTimestamp = function () {
+                    return 123456789;
+                };
+
+                var friends = [
+                    {'username':'test@agical.com', 'timestamp':123456789}
+                ];
+
+                ra.expects('fetchUserData')
+                    .withExactArgs('friendsunhosted_friends')
+                    .returns(resolved(null));
+
+                ra.expects('putUserData')
+                    .withArgs('friendsunhosted_friends', friends)
+                    .returns(resolved(friends));
+
+                fu.addFriend(friends[0].username).then(eq(friends[0]), eq('fail')).always(done);
+
+            },
 
 
-        "- Rejects new friend on error": function(done) {
-            this.originalGetTimestamp = fu.getTimestamp;
-            fu.getTimestamp = function() {return 123456789;};
+            "- Rejects new friend on error":function (done) {
+                this.originalGetTimestamp = fu.getTimestamp;
+                fu.getTimestamp = function () {
+                    return 123456789;
+                };
 
-            var friends = [{'username': 'test@agical.com', 'timestamp':123456789}];
+                var friends = [
+                    {'username':'test@agical.com', 'timestamp':123456789}
+                ];
 
-            ra.expects('fetchUserData')
-                .withExactArgs('friendsunhosted_friends')
-                .returns(rejected(666));
-            
-            fu.addFriend(friends[0].username).then(eq("Should fail"), match("666")).always(done);
-            
-        },
+                ra.expects('fetchUserData')
+                    .withExactArgs('friendsunhosted_friends')
+                    .returns(rejected(666));
 
-});
+                fu.addFriend(friends[0].username).then(eq("Should fail"), match("666")).always(done);
 
-});
+            }
+
+        });
+
+    });
